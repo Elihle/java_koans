84c31f64b558e6e6076695f772a95c5dc13b4349
Elihle <lihlemenz@gmail.com>
2019-04-24 16:00:13 +0200

    initial commit

---
+++ koans/src/beginner/AboutKoans.java
@@ -0,0 +1,26 @@
+package beginner;
+
+import com.sandwich.koan.Koan;
+
+import static com.sandwich.util.Assert.fail;
+
+public class AboutKoans {
+ 
+    @Koan
+    public void findAboutKoansFile() {
+        boolean KoansFileIsFound = true;
+        if (!KoansFileIsFound) {
+            fail("delete this line to advance");
+
+        }
+    }
+
+    @Koan
+    public void definitionOfKoanCompletion() {
+        boolean koanIsComplete = true;
+        if (!koanIsComplete) {
+            fail("what if koanIsComplete variable was true?");
+        }
+    }
+
+}
